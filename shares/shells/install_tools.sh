bea0cc6c0651b9a9a1b88163a123f1823d8ed54a
fuliucansheng <decu@microsoft.com>
2024-12-11 12:05:01 +0800

    add install tools

---
+++ shares/shells/install_tools.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: install_ubuntu.sh
+#
+#         USAGE: ./install_ubuntu.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: fuliucansheng
+#  ORGANIZATION:
+#       CREATED: 04/30/2022 22:39
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+sudo apt update
+sudo apt install -y htop vim tmux zsh
+
+pip3 install jupyterlab gpustat
+mkdir -p ~/.jupyter
+wget https://raw.githubusercontent.com/fuliucansheng/mytools/master/shares/shells/jupyter/config.py -O ~/.jupyter/jupyter_notebook_config.py
